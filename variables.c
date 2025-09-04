feaad7d7dd3d9e97a8d4db2411a7397a354afe03
pdubs3202006-dot <pdubs3202006@gmail.com>
2025-09-04 08:32:07 -0400

    Add files via upload

---
+++ variables.c
@@ -0,0 +1,45 @@
+// Modify this file
+
+#include "variables.h"
+
+/*
+Arguments:
+    number: a integer given from the test case
+    character: a character given from the test case
+Return:
+    return formatted string(char pointer)
+*/
+char* declare_variables(int number, char character)
+{
+    char* answer = malloc(sizeof(char) * 100);
+    
+    
+    // -----------------------------------------------------------
+    // ---- EDIT THIS AREA ONLY, DO NOT EDIT ANY LINES ABOVE -----
+
+    
+    
+    // Variable Declarations
+    int MSEE;
+    int POTR;
+    int STEW;
+    int BHEE;
+    int PMU = 10;
+    
+
+
+    // -------------- DO NOT EDIT ANY LINES BELOW ----------------
+    // -----------------------------------------------------------
+    
+    
+    // Variable Definition
+    MSEE = 10 + number;
+    POTR = 12.4;
+    ELLT = "Chase The River";
+    WALC = -10;
+    CREC = character;
+
+
+    sprintf(answer, "%d%f%s%d%c", MSEE, POTR, ELLT, WALC, CREC);
+    return answer;
+}
